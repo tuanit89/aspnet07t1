bd1fbb893689a51e948a4f1ffc1bf246678dffec
tuan.it89 <tuan.it89@localhost>
2013-07-25 22:04:50 +0700

    Them model

---
+++ Model/Entity/RatingGagInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Model.Entity
+{
+    public class RatingGagInfo
+    {
+        public int RatingGagId { get; set; }
+        public int GagId { get; set; }
+
+        public bool IsInsteresting { get; set; }
+        public bool IsBoring { get; set; }
+    }
+}
---
+++ Model/Entity/UserInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Model.Entity
+{
+    public class UserInfo
+    {
+        public int UserId { get; set; }
+        public int UserName { get; set; }
+
+        public DateTime JoinDate { get; set; }
+        public int TotalGags { get; set; }
+        public int TotalLikes { get; set; }
+        public int TotalComments { get; set; }
+        public int Ranking { get; set; }
+        public string Signature { get; set; }
+
+        public List<GagInfo> ListPostGags(int pageIndex, int pageSize)
+        {
+            return null;
+        } 
+    }
+}

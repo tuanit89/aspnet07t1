f5b2c82af0220d976bd0ef624f8337172b6c5b81
tuan.it89 <tuan.it89@localhost>
2013-07-25 21:21:15 +0700

    Them gaginfo vao model entity

---
+++ Model/Entity/GagInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Model.Entity
+{
+    public class GagInfo
+    {
+        public int Id { get; set; }
+        public string GagName { get; set; }
+        public int GagUserId { get; set; }
+
+        public string GagTypeId { get; set; }
+        public string GagLink { get; set; }
+
+        public DateTime GagCreatedDate { get; set; }
+
+        public int CategoryId { get; set; }
+
+        
+    }
+}
